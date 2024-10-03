0a17440458504b902e86fe810a3cbb051a6a8a73
bruno <brunoquintino.tds2.senai@gmail.com>
2024-10-03 09:01:44 -0300

    refactor: alter server to use routes

--- src/server.js
+++ src/server.js
@@ -1,17 +1,53 @@
-import express from "express";
-import { config } from "dotenv";
+import express from "express"
+import { config } from "dotenv"
 
-config();
+import routes from "./routes/index.routes.js"
+
+config()
 
 const serverPort = process.env.PORT || 3000;
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
+app.use (routes);
 
-app.get("/", (req, res) => {
-  return res.status(200).json({ message: "Hello, World!" });
-});
+const emocoes = [
+    {
+        id: 1,
+        nome: "Raiva",
+        cor: "Vermelho"
+    },
+
+    {
+        id: 2,
+        nome: "Ansiedade",
+        cor: "Laranja"
+    },
+
+    {
+        id: 3,
+        nome: "Alegria",
+        cor: "Amarelo"
+    },
+
+]
+
+
+app.get("/emocoes",(req, res) => {
+    return res.status(200).send( emocoes )
+})
+
+app.post("/emocoes",(req, res) => {
+    const { nome, cor } = req.body
+    const novaEmocao = {
+        id: emocoes.length + 1,
+        nome: nome,
+        cor: cor
+    }
+    emocoes.push(novaEmocao)
+    return res.status(200).send( emocoes )
+})
 
 app.listen(serverPort, () => {
-  console.log(`⚡ Server started on http://localhost:${serverPort}`);
-});
+    console.log(`🎃 Server started on http://localhost:${serverPort}`)
+})
